debf865243c116d605be3405734f7e529ccb5f81
刘欢 <huan.liu@casstime.com>
2024-05-10 14:15:03 +0800

    feat: linkedlist 的操作函数

--- linked-list/main.go
+++ linked-list/main.go
@@ -1,14 +1,107 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	linkedlist "github.com/aniviaH/dsa-go-utils/linked-list/pkg"
+)
 
 func main() {
 	slice := []int{1, 2, 3, 4, 5}
-	// 使用for循环遍历切片
-	for index, value := range slice {
-		fmt.Println(index, value)
-	}
+	slice2 := []int{1, 2, 3, 4}
+	slice3 := []int{7, 8, 9}
 
-	list := NewLinkedListFromSlice(slice)
-	fmt.Println(list)
+	// 使用for循环遍历切片
+	// for index, value := range slice {
+	// 	fmt.Println(index, value)
+	// }
+
+	// 构建链表
+	listHead1 := linkedlist.NewLinkedListFromSlice(slice)
+	listHead2 := linkedlist.NewLinkedListFromSlice(slice2)
+	listHead3 := linkedlist.NewLinkedListFromSlice(slice3)
+
+	// 打印链表的每一项
+	fmt.Println()
+	fmt.Println("======= 打印链表 =======")
+	// fmt.Println(list)
+	linkedlist.PrintLinkedList(listHead1)
+	linkedlist.PrintLinkedList(listHead2)
+
+	// 取中间节点方式1
+	fmt.Println()
+	fmt.Println("======= 获取链表的中间节点 =======")
+	fmt.Println("获取链表中间节点-总数偶数时取中间两个的后者：", slice, "的中间节点为", linkedlist.GetLinkedListMidNode(listHead1))
+	fmt.Println("获取链表中间节点-总数偶数时取中间两个的后者：", slice2, "的中间节点为", linkedlist.GetLinkedListMidNode(listHead2))
+
+	// 取中间节点方式2
+	fmt.Println("获取链表中间节点-总数偶数时取中间两个的前者：", slice, "的中间节点为", linkedlist.GetLinkedListMidNode2(listHead1))
+	fmt.Println("获取链表中间节点-总数偶数时取中间两个的前者：", slice2, "的中间节点为", linkedlist.GetLinkedListMidNode2(listHead2))
+
+	// 取中间节点方式3
+	fmt.Println("获取链表中间节点-总数偶数时取中间两个的前者：", slice, "的中间节点为", linkedlist.GetLinkedListMidNode3(listHead1))
+	fmt.Println("获取链表中间节点-总数偶数时取中间两个的前者：", slice2, "的中间节点为", linkedlist.GetLinkedListMidNode3(listHead2))
+
+	// 链表反转
+	fmt.Println()
+	fmt.Println("======== 反转链表 =======")
+	fmt.Println("---------链表:", slice3, "反转之前-----------")
+	linkedlist.PrintLinkedList(listHead3)
+	listHead3Reversed := linkedlist.ReverseLinkedList(listHead3)
+	fmt.Println("---------链表:", slice3, "反转之后-----------")
+	fmt.Print("原链表头：")
+	linkedlist.PrintLinkedList(listHead3)
+	fmt.Print("新链表头：")
+	linkedlist.PrintLinkedList(listHead3Reversed)
+	fmt.Println()
+
+	// 拼接两个已排序的链表，得到一条排序的链表
+	fmt.Println()
+	slice4 := []int{1, 4, 7}
+	slice5 := []int{3, 6, 9}
+	listHead4 := linkedlist.NewLinkedListFromSlice(slice4)
+	listHead5 := linkedlist.NewLinkedListFromSlice(slice5)
+	fmt.Println("========合并链表", slice4, "和", slice5, "=======")
+	fmt.Print("合并前的", slice4, "链表：")
+	linkedlist.PrintLinkedList(listHead4)
+	fmt.Print("合并后的", slice4, "链表：")
+	linkedlist.PrintLinkedList(listHead5)
+	mergedHead := linkedlist.MergeTwoSortedLinkedList(listHead4, listHead5)
+	fmt.Print("合并后的结果链表：")
+	linkedlist.PrintLinkedList(mergedHead)
+	fmt.Print("合并后的", slice4, "链表：")
+	linkedlist.PrintLinkedList(listHead4)
+	fmt.Print("合并后的", slice5, "链表：")
+	linkedlist.PrintLinkedList(listHead5)
+
+	// 剪裁链表的前n个位置，返回剪裁之后的头部节点
+	fmt.Println()
+	slice6 := []int{1, 2, 3, 4, 5}
+	listHead6 := linkedlist.NewLinkedListFromSlice(slice6)
+	fmt.Print("裁剪前的", slice6, "链表：")
+	linkedlist.PrintLinkedList(listHead6)
+
+	resCut1 := linkedlist.CutLinkedListPreCount(listHead6, 2)
+	fmt.Print("裁剪第一次后的返回结果链表：")
+	linkedlist.PrintLinkedList(resCut1)
+	fmt.Print("裁剪第一次后的", slice6, "链表：")
+	linkedlist.PrintLinkedList(listHead6)
+
+	resCut2 := linkedlist.CutLinkedListPreCount(resCut1, 2)
+	fmt.Print("裁剪第二次后的返回结果链表：")
+	linkedlist.PrintLinkedList(resCut2)
+	fmt.Print("裁剪第二次后的", slice6, "链表：")
+	linkedlist.PrintLinkedList(listHead6)
+	fmt.Print("裁剪第二次后的resCut1链表：")
+	linkedlist.PrintLinkedList(resCut1)
+
+	resCut3 := linkedlist.CutLinkedListPreCount(resCut2, 2)
+	fmt.Print("裁剪第三次后的返回结果链表：")
+	linkedlist.PrintLinkedList(resCut3)
+	fmt.Print("裁剪第三次后的", slice6, "链表：")
+	linkedlist.PrintLinkedList(listHead6)
+	fmt.Print("裁剪第三次后的resCut1链表：")
+	linkedlist.PrintLinkedList(resCut1)
+	fmt.Print("裁剪第三次后的resCut2链表：")
+	linkedlist.PrintLinkedList(resCut2)
 }
--- linked-list/pkg/index.go
+++ linked-list/pkg/index.go
@@ -1,10 +1,13 @@
 package pkg
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+// 以切片构建链表
 func NewLinkedListFromSlice(s []int) *ListNode {
 	// 使用for循环遍历切片
 	dummyNode := &ListNode{Val: -1}
@@ -17,4 +20,145 @@ func NewLinkedListFromSlice(s []int) *ListNode {
 	}
 
 	return dummyNode.Next
+}
+
+// 打印链表
+func PrintLinkedList(head *ListNode) {
+	dummyNode := &ListNode{Next: head}
+
+	cur := dummyNode.Next
+	for cur != nil {
+		fmt.Print(cur.Val)
+		fmt.Print(" -> ")
+		cur = cur.Next
+	}
+
+	fmt.Print("nil")
+	fmt.Println()
+}
+
+// 获取中间节点
+// 链表节点总数为奇数：中间节点
+// 链表节点总数为偶数：中间的两个节点的后面节点 - 对应leetcode的876题：链表的中间节点 [https://leetcode.cn/problems/middle-of-the-linked-list/description/]
+func GetLinkedListMidNode(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+// 获取中间节点
+// 链表节点总数为奇数：中间节点的前面一个节点(奇数场景取到的不是预期的！)
+// 链表节点总数为偶数：中间的两个节点的前面节点 - leetcode的234题：回文链表，用到奇偶场景的正确中间节点的判断 [https://leetcode.cn/problems/middle-of-the-linked-list/description/]
+func GetLinkedListMidNode2(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+	var pre *ListNode
+	for fast != nil && fast.Next != nil {
+		pre = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return pre
+}
+
+// 获取中间节点
+// 链表节点总数为奇数：中间节点
+// 链表节点总数为偶数：中间的两个节点的前面节点 -
+func GetLinkedListMidNode3(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+// 反转整个链表
+func ReverseLinkedList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+
+		pre = cur
+		cur = next
+	}
+
+	return pre
+}
+
+// 合并两条已排序的链表
+func MergeTwoSortedLinkedList(head1, head2 *ListNode) *ListNode {
+	if head1 == nil {
+		return head2
+	}
+	if head2 == nil {
+		return head1
+	}
+
+	dummyNode := &ListNode{Val: -1}
+	pre := dummyNode
+
+	for head1 != nil && head2 != nil {
+		// 谁小将pre指向谁，谁向后移动
+		if head1.Val <= head2.Val {
+			pre.Next = head1
+			head1 = head1.Next
+		} else {
+			pre.Next = head2
+			head2 = head2.Next
+		}
+
+		// 挪动pre指针
+		pre = pre.Next
+	}
+
+	// head1 或 head2 还剩未遍历的
+	if head1 != nil {
+		pre.Next = head1
+	}
+	if head2 != nil {
+		pre.Next = head2
+	}
+
+	return dummyNode.Next
+}
+
+func CutLinkedListPreCount(head *ListNode, n int) *ListNode {
+	cur := head
+
+	cutLoopCount := n - 1
+	for cutLoopCount > 0 && cur != nil {
+		cur = cur.Next
+		cutLoopCount--
+	}
+
+	// 如果裁剪到链表的末尾，这个时候cur为nil了，返回nil
+	if cur == nil {
+		return nil
+	}
+
+	next := cur.Next
+	cur.Next = nil
+
+	return next
 }
