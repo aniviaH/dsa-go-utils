9bbc39ae7abd8dd41af114c2a2a5d1a293a4d3a9
刘欢 <huan.liu@casstime.com>
2024-05-10 10:01:06 +0800

    feat: 链表初始化

--- linked-list/cmd/main.go
+++ linked-list/cmd/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5}
+	// 使用for循环遍历切片
+	for index, value := range slice {
+		fmt.Println(index, value)
+	}
+
+	// list := NewLinkedListFromSlice(slice)
+}
--- linked-list/pkg/index.go
+++ linked-list/pkg/index.go
@@ -4,3 +4,17 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func NewLinkedListFromSlice(s []int) *ListNode {
+	// 使用for循环遍历切片
+	dummyNode := &ListNode{Val: -1}
+	head := dummyNode
+	for _, value := range s {
+		node := &ListNode{Val: value}
+
+		head.Next = node
+		head = head.Next
+	}
+
+	return dummyNode.Next
+}
